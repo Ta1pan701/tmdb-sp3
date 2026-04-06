ce7db125b535984124a8b8b7a049b9234b7c8d84
qwertyaze <amiryaminov2009@gmail.com>
2026-04-06 18:14:03 +0500

    comt

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,5 @@
 import { defineConfig } from "vite"
 
 export default defineConfig({
-    root: './',      // корневая папка проекта (где index.html)
-  base: './public',      // относительные пути для сборки
-  server: {
-    port: 3000,    // порт локального сервера
-    open: true,    // открывать браузер автоматически
-  },
-  build: {
-    outDir: 'dist',   // папка для сборки
-    sourcemap: true,  // чтобы видеть исходники при дебаге
-    rollupOptions: {
-      input: 'index.html', // входной файл
-    },
-  },
-  css: {
-    devSourcemap: true, // source map для CSS
-  },
+    root: '/',
 })
